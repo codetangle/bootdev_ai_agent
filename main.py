6c16791f4a086cac2cb075e6256303f0bd445699
codetangle <trevor@devreason.com>
2025-08-18 21:11:41 -0500

    add calculator

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
+import os
+import sys
+
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
 def main():
-    print("Hello from bootdev-ai-agent!")
+
+    if len(sys.argv) < 2:
+        sys.exit(1)
+    user_prompt = sys.argv[1]
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
+        
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', 
+        contents=messages
+    )
+
+    print(response.text)
+
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {user_prompt}")
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
 
 
 if __name__ == "__main__":
